c59cedf49e472ce935e9b895c1ea75fb5160f051
santiagocarde.v06 <santiagocarde.v06@gmail.com>
2024-11-14 22:42:26 +0000

    Creacion del entorno de desarrollo

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    PINECONE_API_KEY:"pcsk_Ec9Hm_2NsphbtED5DYFgFUwUEmdZqBxTSsvpC1HKP9vfmHcxTah5A83W4KaqvV9aAg85Z",
+    
+};
---
+++ pineconeHandler.js
@@ -0,0 +1,5 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+
+const pc = new Pinecone({
+    apiKey: 'pcsk_Ec9Hm_2NsphbtED5DYFgFUwUEmdZqBxTSsvpC1HKP9vfmHcxTah5A83W4KaqvV9aAg85'
+})
