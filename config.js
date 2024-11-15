e642ce0053f83786a953e7a29dea9b452d5933b9
santiagocarde.v06 <santiagocarde.v06@gmail.com>
2024-11-15 18:36:07 +0000

    .

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    PINECONE_API_KEY:"pcsk_Ec9Hm_2NsphbtED5DYFgFUwUEmdZqBxTSsvpC1HKP9vfmHcxTah5A83W4KaqvV9aAg85Z",
-    
-};
--- pineconeHandler.js
+++
@@ -1,5 +0,0 @@
-import { Pinecone } from "@pinecone-database/pinecone";
-
-const pc = new Pinecone({
-    apiKey: 'pcsk_Ec9Hm_2NsphbtED5DYFgFUwUEmdZqBxTSsvpC1HKP9vfmHcxTah5A83W4KaqvV9aAg85'
-})
